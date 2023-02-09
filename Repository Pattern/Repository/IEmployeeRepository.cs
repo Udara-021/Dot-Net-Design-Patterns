fe51edd59bf3bef116d46f5ff82a8e115e346494
Udara-021 <smartexp7.uh@gmail.com>
2023-02-09 15:56:19 +0530

    Non Generic Repository Pattern Demo

---
+++ Repository Pattern/Repository/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using Repository_Pattern.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository_Pattern.Repository
+{
+   public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+    }
+}
